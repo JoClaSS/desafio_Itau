3138c110c162e9fcd39f485d107ccd7f3cc0377d
José <jclaercio8@gmail.com>
2025-08-12 00:15:49 -0300

    add post endpoint

--- src/main/java/com/desafio/controller/transacaoController.java
+++ src/main/java/com/desafio/controller/transacaoController.java
@@ -3,7 +3,14 @@ package com.desafio.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import com.desafio.dto.transacao;
 
 
 @Controller
@@ -14,5 +21,12 @@ public class transacaoController {
     public ResponseEntity<String> deleteAllTransacao() {
         // Lógica para deletar a transação
         return ResponseEntity.ok("Transações deletadas com sucesso");
+    }
+
+    @PostMapping
+    public ResponseEntity<String> CriarTransacao(@RequestBody transacao transacao) {
+        List<transacao> transacaoList = new ArrayList<>();
+        transacaoList.add(transacao);
+        return ResponseEntity.ok("Transação criada com sucesso");
     }
 }
