6f9f1e286c5c6882249412494f9998719b6164d7
José <jclaercio8@gmail.com>
2025-08-12 00:49:37 -0300

    packing

--- src/main/java/com/desafio/itau/controller/TransacaoController.java
+++ src/main/java/com/desafio/itau/controller/TransacaoController.java
@@ -1,4 +1,4 @@
-package com.desafio.controller;
+package com.desafio.itau.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import com.desafio.itau.dto.*;
 
 import java.util.List;
 import java.util.ArrayList;
-
-import com.desafio.dto.*;
 
 
 @Controller
