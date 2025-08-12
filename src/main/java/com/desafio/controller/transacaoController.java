127adb9b9f65cff6b6b1dd395f18c65e3835edb1
José <jclaercio8@gmail.com>
2025-08-12 00:05:58 -0300

    add endpoint delete

--- src/main/java/com/desafio/controller/transacaoController.java
+++ src/main/java/com/desafio/controller/transacaoController.java
@@ -1,8 +1,15 @@
 package com.desafio.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class transacaoController {
-//git cx
+
+
+    public ResponseEntity<String> deleteAllTransacao() {
+        // Lógica para deletar a transação
+        return ResponseEntity.ok("Transações deletadas com sucesso");
+    }
 }
