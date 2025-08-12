3626b3c93f691d1141cc18bfdde9eb304a64b553
José <jclaercio8@gmail.com>
2025-08-12 00:12:07 -0300

    add annotations

--- src/main/java/com/desafio/controller/transacaoController.java
+++ src/main/java/com/desafio/controller/transacaoController.java
@@ -2,12 +2,15 @@ package com.desafio.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
+@RequestMapping("/transacoes")
 public class transacaoController {
 
-
+    @DeleteMapping
     public ResponseEntity<String> deleteAllTransacao() {
         // Lógica para deletar a transação
         return ResponseEntity.ok("Transações deletadas com sucesso");
